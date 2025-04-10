6d826c71c666df151720c4861cdbd220cd04a459
Timo Bühlmann <timo.buehlmann@gmail.com>
2025-04-10 14:36:29 +0200

    add apply

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export {processIfPresent, ifPresent} from './util'
+export {processIfPresent, ifPresent, apply} from './util'
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,11 @@
-export const processIfPresent = <T, R>(v: T | null | undefined, callback: (v: T) => R): R | undefined =>
+export const processIfPresent = <T, R>
+    (v: T | null | undefined, callback: (v: T) => R)
+    : R | undefined =>
     (v != null && v != undefined) ? callback(v) : undefined
-;
 
 export const ifPresent = processIfPresent
+
+export const apply = <T, R>
+    (v: T, callback: (v: T) => R)
+    : R =>
+    callback(v)
