d5b2d014748233628f6f20b2c494c93189368085
Timo Bühlmann <info@timble.one>
2026-01-07 13:49:06 +0100

    export StoredDataVersionContextProvider

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,6 @@ export {
 } from './util.js'
 
 export {useStoredData} from './use-stored-data/useStoredData.js'
+export {
+    DataVersionContextProvider as StoredDataVersionContextProvider
+} from './use-stored-data/version/DataVersionContextProvider.js'
